e46ec2a579d99f11de87d694d0e3f20bccadfc61
Darya <daryalas17@gmail.com>
2022-08-06 07:35:11 +0300

    seminar4_3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int[] getArr(int NArr)
+{
+
+    int[] A = new int[NArr];
+
+    for(int i = 0; i < A.Length; i++ )
+    {
+           A[i] = new Random().Next(0, 100);
+    }
+
+    return A;
+}
+
+foreach (int item in getArr(8) )
+{
+    Console.Write(item + "  ");
+}
